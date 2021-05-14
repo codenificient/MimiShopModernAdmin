b0b16e0cd941bbb16c10dbeba41c208a9a7105d5
CodenificenT <xristianty@gmail.com>
2021-05-14 01:21:58 -0400

    new build

--- build/static/js/xlsx.5ba791da.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcoreui-free-react-admin-template"]=this["webpackJsonpcoreui-free-react-admin-template"]||[]).push([[9],{826:function(e,t){},906:function(e,t){},907:function(e,t){}}]);
-//# sourceMappingURL=xlsx.5ba791da.chunk.js.map
---
+++ build/static/js/xlsx.5f769b66.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcoreui-free-react-admin-template"]=this["webpackJsonpcoreui-free-react-admin-template"]||[]).push([[10],{855:function(e,t){},950:function(e,t){},951:function(e,t){}}]);
+//# sourceMappingURL=xlsx.5f769b66.chunk.js.map
