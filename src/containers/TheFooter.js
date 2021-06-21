3698962fc1f9fd0c79ae5e15206ff3068d765c09
CodenificienT <xristianty@gmail.com>
2021-06-20 23:37:23 -0400

    link my site and the store in footer

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -4,21 +4,27 @@ import React from 'react';
 const TheFooter = () => {
 	const currentYear = new Date().getFullYear();
 	return (
-		<CFooter fixed={false}>
-			<div>
-				<a href="https://tioye.dev" target="_blank" rel="noopener noreferrer">
-					CodenificenT
-				</a>
-				<span className="ml-1">&copy; 2019 - {currentYear} * All Rights Reserved</span>
-			</div>
-			<div className="mfs-auto">
-				<span className="mr-1">Reviser notre</span>
-				<a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-					Politique de Confidentialité
-				</a>
-			</div>
-		</CFooter>
-	);
+    <CFooter fixed={false}>
+      <div>
+        <a href="https://tioye.dev" target="_blank" rel="noopener noreferrer">
+          CodenificienT
+        </a>
+        <span className="ml-1">
+          &copy; 2019 - {currentYear} * All Rights Reserved
+        </span>
+      </div>
+      <div className="mfs-auto">
+        <span className="mr-1">Reviser notre</span>
+        <a
+          href="https://mimishopouaga.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Voir la Boutique
+        </a>
+      </div>
+    </CFooter>
+  );
 };
 
 export default React.memo(TheFooter);
